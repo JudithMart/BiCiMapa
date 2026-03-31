cc4dd2f1c0d3d09b2a3b7b2acbf682a77fb517e0
Judith Martínez <aguimtz.2003@gmail.com>
2026-03-31 00:16:37 -0600

    Add: Register

--- BiciMapa/src/App.jsx
+++ BiciMapa/src/App.jsx
@@ -1,12 +1,18 @@
-import Login from "./components/Login"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- BiciMapa/src/services/auth.service.js
+++ BiciMapa/src/services/auth.service.js
@@ -8,13 +8,22 @@ Funciones para:
 
 import { supabase } from '../lib/supabase';
 
-export const registerUser = async (email, password) => {
-  const { user, error } = await supabase.auth.signUp({ email, password });
-  return { user, error };
+export const registerUser = async (email, password, nombre, telefono) => {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        nombre,
+        telefono
+      }
+    }
+  });
+  return { user: data?.user, error };
 };
 
 export const loginUser = async (email, password) => {
-  const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+  const { data, error } = await supabase.auth.signInWithPassword({ email, password,});
   return { user: data?.user, error };
 };
 
