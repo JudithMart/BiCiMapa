961d65dd941f875889620973d242d592499f88eb
Judith Martínez <aguimtz.2003@gmail.com>
2026-07-07 16:18:11 -0600

    Fix: scroll to mobile

--- src/admin/components/AdminLayout.jsx
+++ src/admin/components/AdminLayout.jsx
@@ -6,25 +6,26 @@ import AdminNavbar from "./AdminNavbar";
 export default function AdminLayout() {
   return (
     <div
-      className="h-screen overflow-hidden bg-cover bg-center"
+      className="h-dvh overflow-hidden bg-cover bg-center"
       style={{
         backgroundImage: "url('/Fondos/FondoBicis.jpeg')",
       }}
     >
-      <div className="flex h-full overflow-hidden">
+      <div className="flex h-full overflow-hidden  ">
         <AdminNavbar />
 
         <main
-          className="  overflow-y-auto
+          className="  overflow-y-auto overflow-x-hidden 
             flex-1
-            h-full
-            
-            p-6
+            min-w-0
+            h-[calc(100vh-4rem)] md:h-full
+            p-6  md:p-8
             md:ml-0
-            mt-16 md:mt-0
-          "
+            mt-16 md:mt-0 "
         >
-          <Outlet />
+          <div className="max-w-[1400px] mx-auto w-full ">
+            <Outlet />
+          </div>
         </main>
       </div>
     </div>
