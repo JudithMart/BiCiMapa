d425bd47d3053de6b7a763fbac869abde59ae9b4
Judith Martínez <aguimtz.2003@gmail.com>
2026-07-10 22:13:45 -0600

    Fix: translation extension interference with React reconciliation

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
 //main.jsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./patchDomForTranslateExtensions";
 
-import App from './App.jsx'
-import { AuthProvider } from './context/AuthContext';
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext";
+import ErrorBoundary from "./user/components/error/ErrorBoundary.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ErrorBoundary>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ErrorBoundary>
   </StrictMode>,
-)
+);
